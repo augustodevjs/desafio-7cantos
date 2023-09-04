f837d5229819155f2da477e1b6618781adac5360
augusto <augustosaaude@gmail.com>
2023-09-04 12:23:43 -0300

    feat: add global.ts styles

---
+++ frontend/libs/shared/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+    :focus {
+      outline: none;
+      box-shadow: none;
+    }
+  }
+
+  body {
+    background-color: #202024;
+    color: #E1E1E6;
+    -webkit-font-smoothing: antialiased;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem;
+  }
+`;
