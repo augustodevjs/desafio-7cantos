4e19593d19e0b291819a1c6ffd314f387538dadd
augusto <augustosaaude@gmail.com>
2023-09-04 17:29:01 -0300

    feat: update migration

--- backend/database/migrations/2023_09_04_201121_create_tasks_table.php
+++ backend/database/migrations/2023_09_04_201121_create_tasks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->string('descricao');
+            $table->string('responsavel');
             $table->timestamps();
         });
     }
