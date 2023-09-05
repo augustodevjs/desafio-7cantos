6140c405ede5656cbb8fef9dd2a1bc96d5a80f5a
augusto <augustosaaude@gmail.com>
2023-09-05 15:30:14 -0300

    feat: refactor controller

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -4,97 +4,137 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Task;
+use Illuminate\Http\JsonResponse;
 
 class TaskController extends Controller
 {
-    public function getAll()
+    public function getAll(): JsonResponse
     {
-        return Task::orderBy('created_at', 'DESC')->get()->map(function ($task) {
-            $task->completed = (bool) $task->completed;
-            return $task;
-        });
+        try {
+            $tasks = Task::paginate(10);
+            $tasks->getCollection()->transform(function ($task) {
+                return $this->transformCompleted($task);
+            });
+            return response()->json(['data' => $tasks]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Erro ao obter as tarefas'], 500);
+        }
     }
 
-    public function create(Request $request)
+    public function create(Request $request): JsonResponse
     {
-        $newTask = new Task;
-        $newTask->title = $request->item["title"];
-        $newTask->description = $request->item["description"];
-        $newTask->responsible = $request->item["responsible"];
-        $newTask->completed = $request->item["completed"];
-        $newTask->save();
-
-        return $newTask;
+        try {
+            $newTask = Task::create($request->all());
+            return response()->json(['data' => $newTask], 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Erro ao criar a tarefa'], 500);
+        }
     }
 
-    public function getById(string $id)
+    public function getById(string $id): JsonResponse
     {
-        $existingTask = Task::find($id);
+        try {
+            $existingTask = $this->findTaskById($id);
 
-        if(!$existingTask) return 'Tarefa não encontrada';
+            if (!$existingTask) {
+                return $this->taskNotFoundResponse();
+            }
 
-        return $existingTask;
+            return response()->json(['data' => $this->transformCompleted($existingTask)]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Erro ao obter a tarefa'], 500);
+        }
     }
 
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id): JsonResponse
     {
-        $existingTask = Task::find($id);
+        try {
+            $existingTask = $this->findTaskById($id);
 
-        if($existingTask) {
-            $existingTask->title = $request->item["title"];
-            $existingTask->description = $request->item["description"];
-            $existingTask->responsible = $request->item["responsible"];
-            $existingTask->completed = $request->item["completed"];
+            if (!$existingTask) {
+                return $this->taskNotFoundResponse();
+            }
+
+            $existingTask->update($request->all());
+
+            return response()->json(['data' => $this->transformCompleted($existingTask)], 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Erro ao atualizar a tarefa'], 500);
+        }
+    }
+
+    public function uncompleted(string $id): JsonResponse
+    {
+        try {
+            $existingTask = $this->findTaskById($id);
+
+            if (!$existingTask) {
+                return $this->taskNotFoundResponse();
+            }
+
+            if ($existingTask->completed) {
+                $existingTask->update(['completed' => false]);
+                return response()->json(['message' => 'Tarefa marcada como não concluída com sucesso']);
+            }
+
+            return response()->json(['message' => 'A tarefa já está marcada como não concluída']);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Erro ao atualizar o status da tarefa'], 500);
+        }
+    }
+
+    public function completed(string $id): JsonResponse
+    {
+        try {
+            $existingTask = $this->findTaskById($id);
+
+            if (!$existingTask) {
+                return $this->taskNotFoundResponse();
+            }
+
+            if ($existingTask->completed) {
+                return response()->json(['message' => 'Tarefa já está concluída']);
+            }
+
+            $existingTask->completed = true;
             $existingTask->save();
-            return $existingTask;
-        }
 
-        return 'Tarefa não encontrada';
+            return response()->json(['message' => 'Tarefa marcada como concluída com sucesso']);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Erro ao atualizar o status da tarefa'], 500);
+        }
     }
 
-    public function uncompleted(string $id) {
-        $existingTask = Task::find($id);
-
-        if (!$existingTask) {
-            return 'Tarefa não encontrada';
-        }
-
-        if (!$existingTask->completed) {
-            return 'Tarefa já está desconcluída';
-        }
-
-        $existingTask->completed = false;
-        $existingTask->save();
-
-        return 'Tarefa marcada como desconcluída com sucesso';
-    }
-
-    public function completed(string $id) {
-        $existingTask = Task::find($id);
-
-        if (!$existingTask) {
-            return 'Tarefa não encontrada';
-        }
-
-        if ($existingTask->completed) {
-            return 'Tarefa já está concluida';
-        }
-
-        $existingTask->completed = true;
-        $existingTask->save();
-
-        return 'Tarefa marcada como concluída com sucesso';
-    }
-
-    public function delete(string $id)
+    public function delete(string $id): JsonResponse
     {
-        $existingTask = Task::find($id);
+        try {
+            $existingTask = $this->findTaskById($id);
 
-        if($existingTask) {
+            if (!$existingTask) {
+                return $this->taskNotFoundResponse();
+            }
+
             $existingTask->delete();
-            return "Tarefa deleta com sucesso.";
-        }
 
-        return 'Tarefa não encontrada';
+            return response()->json(['message' => 'Tarefa excluída com sucesso']);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Erro ao excluir a tarefa'], 500);
+        }
+    }
+
+    private function findTaskById(string $id)
+    {
+        return Task::find($id);
+    }
+
+    private function taskNotFoundResponse(): JsonResponse
+    {
+        return response()->json(['message' => 'Tarefa não encontrada'], 404);
+    }
+
+    private function transformCompleted(Task $task): Task
+    {
+        $task->completed = (bool) $task->completed;
+        return $task;
     }
 }
