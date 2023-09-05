ddbbb8472c9db4eed9b5732a794e7d2f0879abd1
augusto <augustosaaude@gmail.com>
2023-09-05 18:19:03 -0300

    feat: add documentation of swagger to controllers

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -5,9 +5,69 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Task;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/tasks",
+     *     tags={"Tasks"},
+     *     summary="List all tasks",
+     *     description="Retrieve a paginated list of tasks.",
+     *     @OA\Parameter(
+     *         name="page",
+     *         in="query",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="integer",
+     *             example="Page"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="current_page", type="integer"),
+     *                 @OA\Property(property="data", type="array",
+     *                     @OA\Items(
+     *                         @OA\Property(property="id", type="integer"),
+     *                         @OA\Property(property="title", type="string"),
+     *                         @OA\Property(property="description", type="string"),
+     *                         @OA\Property(property="responsible", type="string"),
+     *                         @OA\Property(property="completed", type="boolean"),
+     *                     ),
+     *                 ),
+     *                 @OA\Property(property="first_page_url", type="string"),
+     *                 @OA\Property(property="from", type="integer"),
+     *                 @OA\Property(property="last_page", type="integer"),
+     *                 @OA\Property(property="last_page_url", type="string"),
+     *                 @OA\Property(property="links", type="array",
+     *                     @OA\Items(
+     *                         @OA\Property(property="url", type="string"),
+     *                         @OA\Property(property="label", type="string"),
+     *                         @OA\Property(property="active", type="boolean"),
+     *                     ),
+     *                 ),
+     *                 @OA\Property(property="next_page_url", type="string"),
+     *                 @OA\Property(property="path", type="string"),
+     *                 @OA\Property(property="per_page", type="integer"),
+     *                 @OA\Property(property="prev_page_url", type="string"),
+     *                 @OA\Property(property="to", type="integer"),
+     *                 @OA\Property(property="total", type="integer"),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal Server Error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="error", type="string"),
+     *         ),
+     *     ),
+     * )
+     */
     public function getAll(): JsonResponse
     {
         try {
@@ -21,8 +81,67 @@ class TaskController extends Controller
         }
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/tasks",
+     *     tags={"Tasks"},
+     *     summary="Create a task",
+     *     description="Create a task for a to-do list",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="title", type="string"),
+     *             @OA\Property(property="description", type="string"),
+     *             @OA\Property(property="responsible", type="string"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="title", type="string"),
+     *                 @OA\Property(property="description", type="string"),
+     *                 @OA\Property(property="responsible", type="string"),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="400",
+     *         description="Bad Request",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Dados Inválidos"),
+     *             @OA\Property(property="errors", type="object",
+     *                 @OA\Property(property="title", type="array",
+     *                     @OA\Items(type="string")
+     *                 ),
+     *     @OA\Property(property="description", type="array",
+     *                      @OA\Items(type="string")
+     *                  ),
+     *     @OA\Property(property="responsible", type="array",
+     *                      @OA\Items(type="string")
+     *                  ),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal Server Error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="error", type="string"),
+     *         ),
+     *     ),
+     * )
+     */
     public function create(Request $request): JsonResponse
     {
+        $validator = $this->validateTaskData($request);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Dados Inválidos', 'errors' => $validator->errors()], 400);
+        }
+
         try {
             $newTask = Task::create($request->all());
             return response()->json(['data' => $newTask], 201);
@@ -31,6 +150,47 @@ class TaskController extends Controller
         }
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/tasks/{id}",
+     *     tags={"Tasks"},
+     *     summary="Get a specific task",
+     *     description="Get a specific task from the list by providing its ID.",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="integer"),
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="title", type="string"),
+     *                 @OA\Property(property="description", type="string"),
+     *                 @OA\Property(property="responsible", type="string"),
+     *                 @OA\Property(property="completed", type="boolean"),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Task not found",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal Server Error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="error", type="string"),
+     *         ),
+     *     ),
+     * )
+     */
     public function getById(string $id): JsonResponse
     {
         try {
@@ -46,8 +206,81 @@ class TaskController extends Controller
         }
     }
 
+    /**
+     * @OA\Put(
+     *     path="/api/tasks/{id}",
+     *     tags={"Tasks"},
+     *     summary="Update a task",
+     *     description="Update an existing task in the to-do list",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="title", type="string"),
+     *             @OA\Property(property="description", type="string"),
+     *             @OA\Property(property="responsible", type="string"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="title", type="string"),
+     *                 @OA\Property(property="description", type="string"),
+     *                 @OA\Property(property="responsible", type="string"),
+     *                 @OA\Property(property="concluded", type="boolean"),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="400",
+     *         description="Bad Request",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Dados Inválidos"),
+     *             @OA\Property(property="errors", type="object",
+     *                 @OA\Property(property="title", type="array",
+     *                     @OA\Items(type="string")
+     *                 ),
+     *                 @OA\Property(property="description", type="array",
+     *                     @OA\Items(type="string")
+     *                 ),
+     *                 @OA\Property(property="responsible", type="array",
+     *                     @OA\Items(type="string")
+     *                 ),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Task not found",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="error", type="string"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal Server Error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="error", type="string"),
+     *         ),
+     *     ),
+     * )
+     */
     public function update(Request $request, string $id): JsonResponse
     {
+        $validator = $this->validateTaskData($request);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Dados Inválidos', 'errors' => $validator->errors()], 400);
+        }
+
         try {
             $existingTask = $this->findTaskById($id);
 
@@ -57,12 +290,47 @@ class TaskController extends Controller
 
             $existingTask->update($request->all());
 
-            return response()->json(['data' => $this->transformCompleted($existingTask)], 201);
+            return response()->json(['data' => $this->transformCompleted($existingTask)], 200);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Erro ao atualizar a tarefa'], 500);
         }
     }
 
+    /**
+     * @OA\Patch(
+     *     path="/api/tasks/{id}/uncompleted",
+     *     tags={"Tasks"},
+     *     summary="Mark a task as uncompleted",
+     *     description="Mark an existing task as uncompleted in the to-do list",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Task not found",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="error", type="string"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal Server Error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="error", type="string"),
+     *         ),
+     *     ),
+     * )
+     */
     public function uncompleted(string $id): JsonResponse
     {
         try {
@@ -72,17 +340,50 @@ class TaskController extends Controller
                 return $this->taskNotFoundResponse();
             }
 
-            if ($existingTask->completed) {
-                $existingTask->update(['completed' => false]);
-                return response()->json(['message' => 'Tarefa marcada como não concluída com sucesso']);
-            }
+            $existingTask->completed = false;
+            $existingTask->save();
 
-            return response()->json(['message' => 'A tarefa já está marcada como não concluída']);
+            return response()->json(['message' => 'Tarefa marcada como não concluída com sucesso']);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Erro ao atualizar o status da tarefa'], 500);
         }
     }
 
+    /**
+     * @OA\Patch(
+     *     path="/api/tasks/{id}/completed",
+     *     tags={"Tasks"},
+     *     summary="Mark a task as completed",
+     *     description="Mark an existing task as completed in the to-do list",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Task not found",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="error", type="string"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal Server Error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="error", type="string"),
+     *         ),
+     *     ),
+     * )
+     */
     public function completed(string $id): JsonResponse
     {
         try {
@@ -90,10 +391,6 @@ class TaskController extends Controller
 
             if (!$existingTask) {
                 return $this->taskNotFoundResponse();
-            }
-
-            if ($existingTask->completed) {
-                return response()->json(['message' => 'Tarefa já está concluída']);
             }
 
             $existingTask->completed = true;
@@ -105,6 +402,41 @@ class TaskController extends Controller
         }
     }
 
+    /**
+     * @OA\Delete(
+     *     path="/api/tasks/{id}",
+     *     tags={"Tasks"},
+     *     summary="Delete a task",
+     *     description="Delete an existing task from the to-do list",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Task not found",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal Server Error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="error", type="string"),
+     *         ),
+     *     ),
+     * )
+     */
     public function delete(string $id): JsonResponse
     {
         try {
@@ -134,7 +466,27 @@ class TaskController extends Controller
 
     private function transformCompleted(Task $task): Task
     {
-        $task->completed = (bool) $task->completed;
+        $task->completed = (bool)$task->completed;
         return $task;
+    }
+
+    private function validateTaskData(Request $request): \Illuminate\Contracts\Validation\Validator
+    {
+        $rules = [
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'responsible' => 'required|string',
+        ];
+
+        $messages = [
+            'title.required' => 'O campo título é obrigatório.',
+            'title.string' => 'O campo título deve ser uma string.',
+            'description.required' => 'O campo descrição é obrigatório.',
+            'description.string' => 'O campo descrição deve ser uma string.',
+            'responsible.required' => 'O campo responsável é obrigatório.',
+            'responsible.string' => 'O campo responsável deve ser uma string.',
+        ];
+
+        return Validator::make($request->all(), $rules, $messages);
     }
 }
