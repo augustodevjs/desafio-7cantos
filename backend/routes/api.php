defc6bbc8897ca647f30354178c756f251551e63
augusto <augustosaaude@gmail.com>
2023-09-04 21:28:13 -0300

    feat: create base TaskController and routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\TaskController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::prefix('/task')->group(function () {
+    Route::post('/store', [TaskController::class, 'store']);
+    Route::put('/{id}', [TaskController::class, 'update']);
+    Route::post('/{id}', [TaskController::class, 'destroy']);
 });
