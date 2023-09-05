00d3be216649f595c5e4c4118a670c081a06be17
augusto <augustosaaude@gmail.com>
2023-09-05 18:18:19 -0300

    feat: add configuration of swagger

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -5,7 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Tasks API",
+ *      description="Desafio Técnico Laravel API Todo"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
